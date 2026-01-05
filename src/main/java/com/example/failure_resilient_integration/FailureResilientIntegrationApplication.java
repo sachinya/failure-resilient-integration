a4e5ca84bda4b5c8f1a74edac443f3ae6e4d6c3d
Sachin Yadav <ysachin704@gmail.com>
2026-01-05 11:13:19 +0530

    Initial failure resilient project

---
+++ src/main/java/com/example/failure_resilient_integration/FailureResilientIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.failure_resilient_integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FailureResilientIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FailureResilientIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/failure_resilient_integration/FailureResilientIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.failure_resilient_integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FailureResilientIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
